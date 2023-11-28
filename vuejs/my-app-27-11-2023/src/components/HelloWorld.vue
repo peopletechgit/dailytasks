ac189ff0263f2d4f7bc5172f2b19f45acc907f90
hari doradla <hari.doradla@peopletech.com>
2023-11-28 12:15:12 +0530

    pushed files

--- vuejs/my-app-27-11-2023/src/components/HelloWorld.vue
+++ vuejs/my-app-27-11-2023/src/components/HelloWorld.vue
@@ -1,58 +1,59 @@
-<template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
-  </div>
-</template>
+<script setup>
+import { ref } from "vue";
+const msg = ref("Hello World!");
+const currentTime = ref("");
+const messages = ref(["Message 1", "Message 2"]);
+const newMessage = ref("");
+const showTime = () => {
+  const now = new Date();
+  const hours = now.getHours();
+  const minutes = now.getMinutes();
+  const seconds = now.getSeconds();
+  currentTime.value = `${hours}:${minutes}:${seconds}`;
+};
 
-<script>
-export default {
-  name: 'HelloWorld',
-  props: {
-    msg: String
+const addMessage = () => {
+  if (newMessage.value.trim() !== "") {
+    messages.value.push(newMessage.value);
+    newMessage.value = "";
   }
-}
+};
+
+const editMessage = (index) => {
+  const editedMessage = prompt("Edit message:", messages.value[index]);
+  if (editedMessage !== null) {
+    messages.value[index] = editedMessage;
+  }
+};
+
+const deleteMessage = (index) => {
+  const confirmDelete = confirm(
+    "Are you sure you want to delete this message?"
+  );
+  if (confirmDelete) {
+    messages.value.splice(index, 1);
+  }
+};
 </script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
-</style>
+<template>
+  <div>
+    <h1>{{ msg }}</h1>
+    <input v-model="msg" />
+    <button @click="showTime">Get Time</button>
+    <p>Show Time: {{ currentTime }}</p>
+
+    <h2>Messages:</h2>
+    <ul>
+      <li v-for="(message, index) in messages" :key="index">
+        {{ message }}
+        <button @click="editMessage(index)">Edit</button>
+        <button @click="deleteMessage(index)">Delete</button>
+      </li>
+    </ul>
+    <div>
+      <input v-model="newMessage" placeholder="New message" />
+      <button @click="addMessage">Add Message</button>
+    </div>
+  </div>
+</template>
