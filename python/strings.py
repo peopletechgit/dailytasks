e5e9aeee59e7a23e311dc26945e83a5403602fdf
hari doradla <hari.doradla@peopletech.com>
2023-11-28 11:39:48 +0530

    created files 28-11-2023

---
+++ python/strings.py
@@ -0,0 +1,39 @@
+# strings : sequence of anything like char, numbers, special symbols donated with ""
+
+# name="suresh"
+# print(name)
+# print(type(name))
+
+# name='rajesh'
+# print(name)
+# print(type(name))
+
+# name="""suresh"""
+# print(name)
+# print(type(name))
+
+# single line '' or ""
+# """""" or """""" ''' ''' -- mutiliple string
+# poem = "balidhan param dharma ha"
+# poem1 = 'balidhan param dharma ha'
+# poem2 = """balidhan param
+# dharma ha"""
+# print(poem ,",", poem1,",",poem2)
+
+# accesing the elements inside the string. -- indexing
+# indexing starts from 0 and will go on depending on length, space also will caluclate as one word
+# [] -- its the symbol for performing the indexing
+str1="python is a programming language."
+# print(str1[0])
+# print(str1[7:20])
+
+#slicing - performng the jumps
+# print(str1[23:0:-1])
+
+# negative indexing  -- it will start from -1 reverse of string
+# print(str1[-1])
+# print(str1[-7])
+# print(str1[0:10:1]) # default value for 3rd argument will be 1
+# print(str1[0:10:2])
+print(str1[-2:-9:1]) #this will return the output as empty since slicing is gooing to perform in positive order but we require it in negative order
+print(str1[-2:-10:-1]) #this will return the output as empty since slicing is gooing to perform in positive order but we require it in negative order
