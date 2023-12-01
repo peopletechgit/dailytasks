ae7ddd3f0eab4707df21d35cfeff3070b633cb20
hari doradla <hari.doradla@peopletech.com>
2023-12-01 18:40:33 +0530

    python strings methods and vue js function with routing

--- vuejs/my-app-27-11-2023/src/components/about.vue
+++ vuejs/my-app-27-11-2023/src/components/about.vue
@@ -1,28 +1,34 @@
 <template>
-    <div>
-        <h2>About Us</h2>
+  <div>
+    <h2>About Us</h2>
 
-        <div class="card">
-            <img src="https://via.placeholder.com/150" alt="Team Member" />
-            <div class="card-body">
-                <h3>John Doe</h3>
-                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam eget turpis ac leo pulvinar.</p>
-            </div>
-        </div>
-
-        <div class="card">
-            <img src="https://via.placeholder.com/150" alt="Team Member" />
-            <div class="card-body">
-                <h3>Jane Doe</h3>
-                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam eget turpis ac leo pulvinar.</p>
-            </div>
-        </div>
+    <div class="card">
+      <img src="https://via.placeholder.com/150" alt="Team Member" />
+      <div class="card-body">
+        <h3>John Doe</h3>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam eget
+          turpis ac leo pulvinar.
+        </p>
+      </div>
     </div>
+
+    <div class="card">
+      <img src="https://via.placeholder.com/150" alt="Team Member" />
+      <div class="card-body">
+        <h3>Jane Doe</h3>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam eget
+          turpis ac leo pulvinar.
+        </p>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
 export default {
-    name: 'AboutPage', // Adjusted to multi-word name
+  name: "AboutPage",
 };
 </script>
 
@@ -30,34 +36,34 @@ export default {
 /* Add your styles here */
 
 h2 {
-    text-align: center;
+  text-align: center;
 }
 
 .card {
-    border: 1px solid #ccc;
-    border-radius: 8px;
-    box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);
-    margin: 20px;
-    overflow: hidden;
-    max-width: 300px;
-    transition: transform 0.3s;
+  border: 1px solid #ccc;
+  border-radius: 8px;
+  box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);
+  margin: 20px;
+  overflow: hidden;
+  max-width: 300px;
+  transition: transform 0.3s;
 }
 
 .card:hover {
-    transform: scale(1.05);
+  transform: scale(1.05);
 }
 
 .card img {
-    width: 100%;
-    height: auto;
-    border-bottom: 1px solid #ccc;
+  width: 100%;
+  height: auto;
+  border-bottom: 1px solid #ccc;
 }
 
 .card-body {
-    padding: 16px;
+  padding: 16px;
 }
 
 h3 {
-    margin-top: 0;
+  margin-top: 0;
 }
 </style>
